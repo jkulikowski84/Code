064ab6e5c0690c7cf7c1a3491432c9c534a58ba1
jkulikowski84 <jkulikowski84@gmail.com>
2024-05-25 14:17:34 -0500

    Update Clear_temps_and_event_logs.bat

--- Batch/Temp-Cleanup/Clear_temps_and_event_logs.bat
+++ Batch/Temp-Cleanup/Clear_temps_and_event_logs.bat
@@ -1,51 +1,51 @@
 @echo off
 CLS
  
-del /s /f /q %windir%\temp\*.*    
-rd /s /q %windir%\temp    
-md %windir%\temp 
+del /s /f /q "%windir%\temp\*.*"
+rd /s /q "%windir%\temp"
+md "%windir%\temp"
    
-del /s /f /q %windir%\Prefetch\*.*    
-rd /s /q %windir%\Prefetch    
-md %windir%\Prefetch
+del /s /f /q "%windir%\Prefetch\*.*"
+rd /s /q "%windir%\Prefetch"
+md "%windir%\Prefetch"
  
-del /s /f /q %windir%\system32\dllcache\*.*    
-rd /s /q %windir%\system32\dllcache    
-md %windir%\system32\dllcache
+del /s /f /q "%windir%\system32\dllcache\*.*"
+rd /s /q "%windir%\system32\dllcache"
+md "%windir%\system32\dllcache"
  
-del /s /f /q %temp%\*.*    
-rd /s /q %temp%    
-md %temp%
+del /s /f /q "%temp%\*.*"
+rd /s /q "%temp%"
+md "%temp%"
   
-del /s /f /q "%USERPROFILE%\Local Settings\History"\*.*    
+del /s /f /q "%USERPROFILE%\Local Settings\History\*.*"
 rd /s /q "%USERPROFILE%\Local Settings\History"    
 md "%USERPROFILE%\Local Settings\History"    
 
-del /s /f /q "%USERPROFILE%\Local Settings\Temporary Internet Files"\*.*    
+del /s /f /q "%USERPROFILE%\Local Settings\Temporary Internet Files\*.*"
 rd /s /q "%USERPROFILE%\Local Settings\Temporary Internet Files"    
 md "%USERPROFILE%\Local Settings\Temporary Internet Files"    
 
-del /s /f /q "%USERPROFILE%\Local Settings\Temp"\*.*    
+del /s /f /q "%USERPROFILE%\Local Settings\Temp\*.*"
 rd /s /q "%USERPROFILE%\Local Settings\Temp"    
 md "%USERPROFILE%\Local Settings\Temp"    
 
-del /s /f /q "%USERPROFILE%\Recent"\*.*    
+del /s /f /q "%USERPROFILE%\Recent\*.*"
 rd /s /q "%USERPROFILE%\Recent"    
 md "%USERPROFILE%\Recent"    
 
-del /s /f /q "%USERPROFILE%\Cookies"\*.*    
+del /s /f /q "%USERPROFILE%\Cookies\*.*"
 rd /s /q "%USERPROFILE%\Cookies"    
 md "%USERPROFILE%\Cookies"
 
-deltree /y c:\windows\tempor~1
-deltree /y c:\windows\temp
-deltree /y c:\windows\tmp
-deltree /y c:\windows\ff*.tmp
-deltree /y c:\windows\history
-deltree /y c:\windows\cookies
-deltree /y c:\windows\recent
-deltree /y c:\windows\spool\printers
-del c:\WIN386.SWP
+deltree /y "c:\windows\tempor~1"
+deltree /y "c:\windows\temp"
+deltree /y "c:\windows\tmp"
+deltree /y "c:\windows\ff*.tmp"
+deltree /y "c:\windows\history"
+deltree /y "c:\windows\cookies"
+deltree /y "c:\windows\recent"
+deltree /y "c:\windows\spool\printers"
+del "c:\WIN386.SWP"
 
 CLS
 
