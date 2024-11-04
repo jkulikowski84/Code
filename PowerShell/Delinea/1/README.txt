6ef36aff90a0bb47230bfc3789fc6b33f0791980
Kulikowski <jkulikowski@nch.org>
2024-11-04 17:11:52 -0600

    Added Delinea 1

---
+++ PowerShell/Delinea/1/README.txt
@@ -0,0 +1,6 @@
+This script uses the "Thycotic.SecretServer" PowerShell module.
+
+Connect to the thycotic server for your organization and validate it with the token.
+You can get the token by going to your user preferences in Delinea/Thycotic and then Generate the API Token and copy to clipboard.
+
+Depending on your organization this token can refresh every few hours or every few days/weeks, so when you use this as an authentication method it can be very helpful for automation.
---
+++ PowerShell/Delinea/1/RefreshAdminPassword.ps1
@@ -0,0 +1,73 @@
+﻿CLS
+
+if((Get-Module -ListAvailable -Name "Thycotic.SecretServer") -or (Get-Module -Name "Thycotic.SecretServer"))
+{
+        #Import-Module Thycotic.SecretServer
+}
+else
+{	
+    Install-Module -Name Thycotic.SecretServer -RequiredVersion 0.61.0 -Scope CurrentUser -Force -Confirm:$False
+	Import-Module Thycotic.SecretServer
+}
+
+if($NULL -eq $session)
+{
+    $token = "longencryptionkey"
+    
+    Try
+    {
+        $session = New-TssSession https://thycotic.domain.com/SecretServer -AccessToken $token -WarningAction Stop
+    }
+    Catch
+    {
+        #Write-Output "Token expired. Replace your token."
+        Add-Type -AssemblyName PresentationCore,PresentationFramework
+        $ButtonType = [System.Windows.MessageBoxButton]::OK
+        $MessageIcon = [System.Windows.MessageBoxImage]::Information
+        $MessageBody = "Token expired. Replace your token."
+        $MessageTitle = "Token Expired"
+        $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+
+        if($NULL -ne $Result)
+        {
+            Exit
+        }
+    }
+}
+
+#=========== Get New Admin PW from Thycotic
+
+$SecretID = (Thycotic.SecretServer\Find-TssSecret -TssSession $session -SearchText "SomeAccountPassWord" | Select-Object | Where-Object { $_.SecretName -notlike "*dmz*"}).SecretId
+
+$NewPW = ((Get-TssSecret -TssSession $session -Id $SecretID).items | where-object {$_.FieldName -eq "Password"}).ItemValue
+
+#=========== Get Old Admin PW from QuickTextPaste
+
+#Location of the config file we want to modify
+$QTP = "C:\Files\QuickTextPaste\QuickTextPaste.ini"
+
+#Read the content of the file
+$Content = [System.IO.File]::ReadAllLines($QTP)
+
+#Get the line we want to work with
+$line = $Content | Select-Object | Where-Object {$_ -like "*L-Win+E*"}
+
+#Pull the current password from the file
+$OldPW = (($line -split ('-p '))[1]).split(' ')[0]
+
+#Replace Old Password with new one
+$NewContent = $Content.Replace("$OldPW","$NewPW")
+
+[System.IO.File]::WriteAllLines("$QTP", $NewContent, [System.Text.Encoding]::Unicode)
+
+#Restart QTP Client
+$proc = Get-Process -Name QuickTextPaste_x64_p | Sort-Object -Property ProcessName -Unique
+
+if($NULL -ne $proc)
+{
+    $proc.Kill()
+    Start-Sleep -s 1
+}
+
+#Restart our process
+Invoke-Item "C:\Files\QuickTextPaste\QuickTextPaste_x64_p.exe"
